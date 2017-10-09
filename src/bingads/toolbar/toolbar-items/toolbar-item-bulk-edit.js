4e1989dbc826bbf805b12b899e70ed5e8fd8d91c
touchskyer <touchskyer@touchskyers-Mac-mini.local>
2017-10-09 16:42:43 +0800

    Imported BingAds its own glyph icons Applied the Icon to bulk edit dropdown to prove it works

--- src/bingads/toolbar/toolbar-items/toolbar-item-bulk-edit.js
+++ src/bingads/toolbar/toolbar-items/toolbar-item-bulk-edit.js
@@ -7,9 +7,9 @@ class ToolbarItemBulkEdit extends Component {
                 <button className="btn btn-default" data-toggle="dropdown" >Edit<span className="caret"></span>
                 </button>
                 <ul className="dropdown-menu"> 
-                    <li><a href="javascript:void(0)" onClick={() => {this.props.callback('BulkEditEnable')}}><span className="glyphicon glyphicon-play"></span>&nbsp;Enable</a></li>
-                    <li><a href="javascript:void(0)"><span className="glyphicon glyphicon-pause"></span>&nbsp;Pause</a></li>
-                    <li><a href="javascript:void(0)"><span className="glyphicon glyphicon-stop"></span>&nbsp;Delete</a></li>
+                    <li><a href="javascript:void(0)" onClick={() => {this.props.callback('BulkEditEnable')}}><span className="bingads-icon-status-ok"></span>&nbsp;Enable</a></li>
+                    <li><a href="javascript:void(0)"><span className="bingads-icon-status-paused"></span>&nbsp;Pause</a></li>
+                    <li><a href="javascript:void(0)"><span className="bingads-icon-status-delete"></span>&nbsp;Delete</a></li>
                     <li className="divider"></li>
                     <li><a href="javascript:void(0)"  onClick={() => {this.props.callback('BulkEditChangeBudget')}}>Change budgets</a></li>
                     <li><a href="javascript:void(0)">Other changes</a></li>
